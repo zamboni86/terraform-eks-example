7927dddd96fdd2c68e705f69788cebe420d47aaa
zamboni86 <zanoni.contreras@gmail.com>
2024-05-05 16:27:39 -0600

    test new env

---
+++ eks/example/providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.47.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.10.1"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.21.1"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "1.14.0"
+    }
+  }
+  required_version = "1.3.2"
+}
